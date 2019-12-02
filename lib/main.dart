01ec8a46b007816558a423c16c5610366fe4cc17
champikamendis <37321146+champikamendis@users.noreply.github.com>
2019-12-03 02:59:28 +0530

    Flutter BMI app

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_app_challenge1/ui/home.dart';
+import 'package:flutter/material.dart';
+void main() {
+  runApp(new MaterialApp(
+    title:'BMI',
+    home: new Bmi(),
+  ));
+
+}
